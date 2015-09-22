2f8798841abd40b3d06ea975d9aa25a57901e381
Stuart Corbishley <corbish@gmail.com>
2015-09-22 10:29:41 +0200

    Mount /mappings onto App

--- lib/app.rb
+++ lib/app.rb
@@ -1,15 +1,23 @@
 require 'inbox'
 require 'receipts'
+require 'mappings'
 
 class App < Roda
 
   route do |r|
+
     r.on "receipts" do 
       r.run Receipts
     end
+
     r.on "inbox" do 
       r.run Inbox
     end
+
+    r.on "mappings" do 
+      r.run Mappings
+    end
+
   end
 
 end
